3f806e99621145c9fa9fd1c8ac074cca61059743
SuperUser <denisedikh@yandex.ru>
2024-06-27 19:21:15 +0300

    1. Добавить счетчик и откорректировать метод по его увеличению 2. Убрать метод валидации объекта, т.к. валидируется при помощи аннотаций 3. Добавить модификатор final у мапы

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -10,11 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,8 +21,8 @@ import java.util.Map;
 @Slf4j
 public class FilmController {
 
-    private Map<Long, Film> films = new HashMap<>();
-    private static final LocalDate DATE_OF_BIRTHDAY_FILM = LocalDate.of(1895, Month.DECEMBER, 28);
+    private final Map<Long, Film> films = new HashMap<>();
+    private long counterId = 0L;
 
     @GetMapping
     public Collection<Film> getAll() {
@@ -34,7 +31,6 @@ public class FilmController {
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
-        //checkFilm(film);
         film.setId(getNextId());
         films.put(film.getId(), film);
         log.info("Добавили фильм {}", film);
@@ -48,7 +44,6 @@ public class FilmController {
             throw new ConditionsNotMetException("id должен быть указан");
         }
         if (films.containsKey(newFilm.getId())) {
-            //checkFilm(newFilm);
             Film oldFilm = films.get(newFilm.getId());
             oldFilm.setName(newFilm.getName());
             oldFilm.setDescription(newFilm.getDescription());
@@ -63,22 +58,7 @@ public class FilmController {
 
     }
 
-    void checkFilm(Film film) {
-        if (film.getName() == null || film.getName().isBlank()
-                || film.getDescription() == null || film.getDescription().length() > 200
-                || film.getReleaseDate() == null || film.getReleaseDate().isBefore(DATE_OF_BIRTHDAY_FILM)
-                || film.getDuration() == null || film.getDuration() <= 0) {
-            log.warn("Не выполнены условия");
-            throw new ValidationException("Фильм не прошел проверку");
-        }
-    }
-
     private Long getNextId() {
-        long counterId = films.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
         return ++counterId;
     }
 }
